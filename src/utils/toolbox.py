5fd61f67f9bc9ebab27376477cca924598723690
Greensky <david.hesliere@gmail.com>
2024-10-31 16:10:28 +0100

    chore: 🔨 Ajout d'une fonction de parsing de position

--- src/utils/toolbox.py
+++ src/utils/toolbox.py
@@ -1,3 +1,7 @@
+from typing import Literal
+
+from src.utils.globals import env
+
 def parse_integer(number: any, default = 0) -> int:
     """
     Renvoie l'entrée sous forme d'entier si possible, sinon renvoie la valeur par défaut.
@@ -21,3 +25,13 @@ def parse_float(number: any, default = 0.0) -> float:
         return float(number)
     except ValueError:
         return default
+
+def parse_position(position: int, dimension: Literal["width", "height"] = "width") -> int:
+    """
+    Renvoie la position sous forme d'entier positif, encadrée par les dimensions du jeu.
+
+    :param position: int - La position à convertir.
+    :param dimension: Literal["width", "height"] - La dimension à laquelle la position doit être encadrée.
+    :return: int
+    """
+    return max(min(position, env["width" if dimension == "width" else "height"]), 0)
