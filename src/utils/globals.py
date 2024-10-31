f1ae1ce6fafd8f500370212e0534a8d4fe7af473
Greensky <david.hesliere@gmail.com>
2024-10-31 15:56:23 +0100

    chore: 🔧 Modification du fichier de variables globales

--- src/utils/globals.py
+++ src/utils/globals.py
@@ -1,7 +1,7 @@
 # Variables à utiliser dans tout le projet
+# Les variables sont définies ici sous forme de classe pour éviter les problèmes de portée, et pour avoir l'auto complétion dans les IDE, ainsi que la vérification de type/erreurs pré-exécution
 
 ## Variables relatives au joueur
-player_variables = {
-    "max_health": 100,
-    "min_health": 0
-}
+class player_variables:
+    max_health =  100,
+    min_health = 0
