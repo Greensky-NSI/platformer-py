43baa49cac5468849c0e51c72fb3c19c69fe663c
Greensky <david.hesliere@gmail.com>
2024-11-01 16:50:34 +0100

    refactor: 🧱 Extraction de variables dans les globales

--- src/utils/globals.py
+++ src/utils/globals.py
@@ -1,6 +1,13 @@
 # Variables à utiliser dans tout le projet
 # Les variables sont définies ici sous forme de classe pour éviter les problèmes de portée, et pour avoir l'auto complétion dans les IDE, ainsi que la vérification de type/erreurs pré-exécution
 
+## Couleurs
+
+class couleurs:
+    decoration_border_color = (128, 128, 128)
+    decoration_wall_color = (0, 12, 20)
+    background_fill_color = 200
+
 ## Variables relatives au joueur
 class movements:
     left = ["q", "a", "left"]
@@ -20,3 +27,4 @@ class player_variables:
 class env:
     width = 600
     height = 800
+    wall_width = 200
