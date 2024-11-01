b205532b38c4d1d73630d52b1b3340c6ebbbd7de
Greensky <david.hesliere@gmail.com>
2024-11-01 17:30:03 +0100

    feat: 🚀 Utilisation du ticker dans le main

--- src/main.py
+++ src/main.py
@@ -3,18 +3,21 @@ from p5 import *
 from src.classes.environnement.decoration import decoration
 from src.custom_types import *
 
+from src.utils.variables import game_ticker
 from src.utils.globals import env, player_variables
 from src.classes.mobs.player import Player
 
 def setup():
     size(env.width, env.height)
+    game_ticker.register("game_start")
 
 player = Player("Greensky", 100)
 
 def draw():
+    game_ticker.tick()
     decoration()
 
-    # player.display()
+    player.display()
     if key_is_pressed:
         pressed = str(key).lower()
 
