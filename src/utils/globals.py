02d319f542979ec1a2138523aa544fb473657bdb
Greensky <david.hesliere@gmail.com>
2024-11-07 20:15:54 +0100

    chore: 🚚 Extraction du design du joueur dans un fichier design.playerDesign

--- src/utils/globals.py
+++ src/utils/globals.py
@@ -1,5 +1,7 @@
 # Variables à utiliser dans tout le projet
 # Les variables sont définies ici sous forme de classe pour éviter les problèmes de portée, et pour avoir l'auto complétion dans les IDE, ainsi que la vérification de type/erreurs pré-exécution
+from p5 import Color
+
 
 ## Variables relatives au joueur
 class movements:
@@ -19,6 +21,11 @@ class player_variables:
     player_colors = {
         "hero_color": (30, 144, 255),  # Couleur principale
         "body_color": (0, 128, 128),  # Couleur du corps
+        "head_color": (0, 0, 0),
+        "left_leg_color": (0, 0, 255),
+        "right_leg_color": (255, 255, 255),
+        "left_arm_color": (255, 0, 0),
+        "right_arm_color": (0, 255, 0),
         "detail_color": (255, 215, 0)
     }
 
@@ -26,3 +33,5 @@ class player_variables:
 class env:
     width = 600
     height = 800
+
+
