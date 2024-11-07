c8b98e5a0f02649d9b9c30c7e1f169f218dd55ac
Greensky <david.hesliere@gmail.com>
2024-11-07 20:18:22 +0100

    chore: 🚀 Introduction du dessin dans la classe joueur

--- src/classes/mobs/player.py
+++ src/classes/mobs/player.py
@@ -1,7 +1,9 @@
-from src.utils.toolbox import parse_integer, parse_position, safe_stroke, safe_fill
+from src.utils.toolbox import parse_integer, parse_position
 from src.utils.globals import player_variables
 from src.types.movement import Direction
-from p5 import rect, translate
+from p5 import translate, scale
+
+from src.Designs.playerDesign import draw_face
 
 class Player:
     def __init__(self, name, health):
@@ -18,25 +20,7 @@ class Player:
 
     def display(self):
         translate(self._x, self._y)
-        # Tête
-        safe_fill(player_variables.player_colors["hero_color"])
-        rect((24, 8), 16, 16)
 
-        # Corps
-        safe_fill(player_variables.player_colors["body_color"])
-        rect(22, 24, 20, 24)
-
-        # Bras
-        rect(14, 26, 8, 18)  # bras gauche
-        rect(42, 26, 8, 18)  # bras droit
-
-        # Jambes
-        safe_fill(player_variables.player_colors["hero_color"])
-        rect(24, 48, 8, 12)  # jambe gauche
-        rect(32, 48, 8, 12)  # jambe droite
-
-        # Détails : ceinture
-        safe_fill(player_variables.player_colors["detail_color"])
-        rect(28, 40, 8, 4)
+        draw_face(1.2)
 
         translate(-self._x, -self._y)
