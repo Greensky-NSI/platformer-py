19474f2b32caafcb57d8c262fad7d191919e6134
greensky-gs <david.hesliere@gmail.com>
2024-11-08 20:38:00 +0100

    chore: 🏷️ Ajout du type des cadeaux dans les entités du DOM

--- src/classes/typeClasses/DOM.py
+++ src/classes/typeClasses/DOM.py
@@ -1,10 +1,11 @@
 from typing import List
 from src.classes.mobs.player import Player
+from src.classes.statics.Cadeau import Cadeau
 
 # à compléter avec les classes qui correspondent
 class EntitiesType:
     players: List[Player]
     platforms: List
     monsters: List
-    gifts: List
+    gifts: List[Cadeau]
     doors: List
