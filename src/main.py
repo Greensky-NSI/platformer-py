99a0d5bf0bc2541802dc9f6857faa29c9b580445
greensky-gs <david.hesliere@gmail.com>
2024-11-08 19:45:28 +0100

    feat: 🚀 Utilisation du DOM dans le fichier principal

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from p5 import *
 from src.classes.environnement.decoration import decoration
 from src.custom_types import *
 
-from src.utils.variables import game_ticker
+from src.utils.variables import game_ticker, game_DOM
 from src.utils.globals import env, player_variables
 from src.classes.mobs.player import Player
 
@@ -13,9 +13,13 @@ def setup():
 
 player = Player("Greensky", 100)
 
+game_DOM.add_player_in_dom(player)
+
 def draw():
     game_ticker.tick()
     decoration()
+
+    game_DOM.tick()
 
     player.display()
     if key_is_pressed:
@@ -26,6 +30,8 @@ def draw():
         elif pressed in player_variables.deplacements_keymap.right:
             player.move("right")
         elif pressed in player_variables.deplacements_keymap.up:
-            pass
+            player.jump(game_ticker)
+    if mouse_is_pressed:
+        player.jump(game_ticker)
 
 run()
--- src/utils/variables.py
+++ src/utils/variables.py
@@ -1,3 +1,5 @@
 from src.abstract.Ticker import Ticker
+from src.classes.core.DOM import DOM
 
 game_ticker = Ticker()
+game_DOM = DOM(ticker=game_ticker)
