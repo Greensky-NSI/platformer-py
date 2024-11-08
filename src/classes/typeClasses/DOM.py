ef65533afd8827494161272f7528c0b267ddd567
greensky-gs <david.hesliere@gmail.com>
2024-11-08 19:44:07 +0100

    feat: 🏷️ Ajout du fichier de typage des entités du DOM

---
+++ src/classes/typeClasses/DOM.py
@@ -0,0 +1,10 @@
+from typing import List
+from src.classes.mobs.player import Player
+
+# à compléter avec les classes qui correspondent
+class EntitiesType:
+    players: List[Player]
+    platforms: List
+    monsters: List
+    gifts: List
+    doors: List
