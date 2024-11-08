6efe64c1c37fed31077b5b05ffa261223dd31191
greensky-gs <david.hesliere@gmail.com>
2024-11-08 20:35:50 +0100

    feat: 🔧 Ajout des variables relatives aux cadeaux

--- src/utils/globals.py
+++ src/utils/globals.py
@@ -2,11 +2,16 @@
 # Les variables sont définies ici sous forme de classe pour éviter les problèmes de portée, et pour avoir l'auto complétion dans les IDE, ainsi que la vérification de type/erreurs pré-exécution
 
 ## Couleurs
+class couleurs_cadeau:
+    couleur_principale = (121, 33, 255)
+    couleur_ruban = (255, 213, 61)
 
 class couleurs:
     decoration_border_color = (128, 128, 128)
     decoration_wall_color = (0, 12, 20)
     background_fill_color = 200
+
+    couleurs_cadeau: couleurs_cadeau
 
 ## Variables relatives au joueur
 class movements:
@@ -31,3 +36,12 @@ class env:
     wall_width = 20
     wall_border_stroke = 2
     wall_total_width = wall_width + wall_border_stroke
+
+## Variables relatives au objets
+class variables_cadeau:
+    couleurs = couleurs_cadeau
+
+    size = 50
+    tailles_noeuds = [20, 25]
+
+    epaisseur_ruban = 8
