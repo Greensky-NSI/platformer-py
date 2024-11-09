ca97d7de718d90e032b17dfaa33baa02935faff0
greensky-gs <david.hesliere@gmail.com>
2024-11-09 12:25:31 +0100

    fix: 🔨 Réglage de la variable de saut du joueur

--- src/utils/globals.py
+++ src/utils/globals.py
@@ -27,7 +27,7 @@ class player_variables:
     width = 30
     height = 52
     delta_speed = 5
-    falling_delta_speed = int(delta_speed * .8)
+    falling_delta_speed = int(delta_speed * 1)
 
     deplacements_keymap = movements
 
