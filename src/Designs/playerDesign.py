7346512835180840f62b7f964e31eed83ddfc923
greensky-gs <david.hesliere@gmail.com>
2024-11-09 11:15:49 +0100

    fix: 💄 Adaptation du design du joueur au translate

--- src/Designs/playerDesign.py
+++ src/Designs/playerDesign.py
@@ -4,31 +4,31 @@ from src.utils.toolbox import safe_fill
 
 def draw_head(i=1):
     safe_fill(player_variables.player_colors["head_color"])
-    rect(24*i, 8*i, 16*i, 16*i)
+    rect(8*i, -36*i, 16*i, -16*i)
 
 def draw_body(i=1):
     safe_fill(player_variables.player_colors["body_color"])
-    rect(22*i, 24*i, 20*i, 24*i)
+    rect(8*i, -12*i, 16*i, -24*i)
 
 def draw_left_arm(i=1):
     safe_fill(player_variables.player_colors["left_arm_color"])
-    rect(14*i, 26*i, 8*i, 18*i)
+    rect(0, -12*i, 8*i, -24*i)
 
 def draw_right_arm(i=1):
     safe_fill(player_variables.player_colors["right_arm_color"])
-    rect(42*i, 26*i, 8*i, 18*i)
+    rect(24*i, -12*i, 8*i, -24*i)
 
 def draw_left_leg(i=1):
     safe_fill(player_variables.player_colors["left_leg_color"])
-    rect(24*i, 48*i, 8*i, 12*i)
+    rect(8*i, 0, 8*i, -12*i)
     
 def draw_right_leg(i=1):
     safe_fill(player_variables.player_colors["right_leg_color"])
-    rect(32*i, 48*i, 8*i, 12*i)
+    rect(16*i, 0, 8*i, -12*i)
 
 def draw_belt(i=1):
     safe_fill(player_variables.player_colors["detail_color"])
-    rect(28*i, 40*i, 8*i, 4*i)
+    rect(11.5*i, -12*i, 10*i, -6*i)
 
 def draw_face(i=2):
     draw_head(i)
