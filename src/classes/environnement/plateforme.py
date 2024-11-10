357d8876e097b9ee0c09054224e5bb0af9ace802
Axelwlr <axel.willier@gmail.com>
2024-11-10 17:43:44 +0100

    Update plateforme.py

--- src/classes/environnement/plateforme.py
+++ src/classes/environnement/plateforme.py
@@ -1,5 +1,5 @@
 class Plateforme:
-    def __init__(self, x, y, largeur, hauteur):
+    def __init__(self, x, y, largeur, hauteur, couleur):
         """
         Initialise une nouvelle plateforme.
 
@@ -8,33 +8,35 @@ class Plateforme:
             y (int): Position y de la plateforme.
             largeur (int): Largeur de la plateforme.
             hauteur (int): Hauteur de la plateforme.
+            couleur (tuple): couleur de la plateforme.
         """
         self.x = x
         self.y = y
         self.largeur = largeur
         self.hauteur = hauteur
+        self.couleur = couleur
 
     def afficher(self):
         """
-        Affiche la plateforme sur l'écran (méthode utile pour un jeu utilisant une bibliothèque)
+        Affiche la plateforme sur l'écran
         """
         self.couleur = (139, 69, 19)
         safe_fill(self.couleur)# couleur marron pour représenter une plateforme classique
         rect(self.x, self.y, self.largeur, self.hauteur)
 
-    def collision(self, joueur):
+    def collision(self, mob):
         """
         Vérifie si le joueur entre en collision avec la plateforme.
 
         Args:
-            joueur: Un objet représentant le joueur avec des attributs de position et de taille.
+            mob: Un objet représentant le joueur (ou un monstre) avec des attributs de position et de taille.
 
         Return:
             bool: True si le joueur est en collision avec la plateforme, sinon False.
         """
         return (
-            joueur.x + joueur.largeur > self.x and
-            joueur.x < self.x + self.largeur and
-            joueur.y + joueur.hauteur > self.y and
-            joueur.y < self.y + self.hauteur
+            mob.x + mob.largeur > self.x and
+            mob.x < self.x + self.largeur and
+            mob.y + mob.hauteur > self.y and
+            mob.y < self.y + self.hauteur
         )
