b2f3375cbe2d35d7753bfaf9c43d9f4c85705524
greensky-gs <david.hesliere@gmail.com>
2024-11-10 18:33:29 +0100

    fix: 💄 Adaptation du design du joueur de profil au translate

--- src/Designs/playerDesign.py
+++ src/Designs/playerDesign.py
@@ -41,33 +41,33 @@ def draw_face(i=2):
 
 def draw_head_profile(i=1, is_left=False):
     safe_fill(player_variables.player_colors["head_color"])
-    x_offset = -4 * i if is_left else 4 * i
     if is_left:
-        rect((22 * i + x_offset, 8 * i), 16 * i, 16 * i)
+        rect(8*i, -36*i, 16*i, -16*i)
     else:
-        rect((22 * i, 8 * i), 16 * i, 16 * i)# Tête en profil
+        rect(8*i, -36*i, 16*i, -16*i)# Tête en profil
 
 def draw_body_profile(i=1, is_left=False):
     safe_fill(player_variables.player_colors["body_color"])
-    rect((22 * i, 24 * i), 12 * i, 24 * i)  # Corps de profil
+    rect(8*i, -12*i, 16*i, -24*i)  # Corps de profil
 
 def draw_arm_profile(i=1, is_left=False):
     safe_fill(player_variables.player_colors["left_arm_color"] if is_left else player_variables.player_colors["right_arm_color"])
-    rect((24 * i, 26 * i), 8 * i, 18 * i)  # Bras en profil
+    rect(10*i, -14*i, 12*i, -20*i)  # Bras en profil
 
 def draw_leg_profile(i=1, is_left=False):
+    safe_fill(player_variables.player_colors["right_leg_color"] if is_left else player_variables.player_colors["left_leg_color"])
+    rect(14*i, 0, 8*i, -12*i)  # Jambe arrière en profil
+
     safe_fill(player_variables.player_colors["left_leg_color"] if is_left else player_variables.player_colors["right_leg_color"])
-    rect((24 * i, 48 * i), 8 * i, 12 * i)  # Jambe en profil
+    rect(8*i, 0, 8*i, -12*i)  # Jambe avant en profil
 
 def draw_right_profile(i=1):
-    background(220)
     draw_head_profile(i)
     draw_body_profile(i)
     draw_arm_profile(i)
     draw_leg_profile(i)
 
-def draw_left_profile(i=1 ):
-    background(220)
+def draw_left_profile(i=1):
     draw_head_profile(i, is_left=True)
     draw_body_profile(i, is_left=True)
     draw_arm_profile(i, is_left=True)
