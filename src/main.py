e2dbf87f9763b29140c7e22d3d23d9c170cae151
Greensky <david.hesliere@gmail.com>
2024-11-26 20:57:39 +0100

    refactor: 🚚 Extraction du joueur dans le fichier variables

--- src/main.py
+++ src/main.py
@@ -1,18 +1,15 @@
 from p5 import *
 
 from src.Designs.decoration import decoration
-from src.classes.mobs.player import Player
 from src.custom_types import *
 from src.utils.globals import env, player_variables
-from src.utils.variables import game_ticker, game_DOM
+from src.utils.variables import game_ticker, game_DOM, player
 
 
 def setup():
     size(env.width, env.height)
     game_ticker.register("game_start")
 
-
-player = Player("Greensky", 100)
 
 game_DOM.add_player_in_dom(player)
 
--- src/utils/variables.py
+++ src/utils/variables.py
@@ -1,5 +1,10 @@
 from src.abstract.Ticker import Ticker
 from src.classes.core.DOM import DOM
+from src.classes.mobs.player import Player
+
+from src.levels import one
+
+player = Player("Greensky", 100)
 
 game_ticker = Ticker()
 game_DOM = DOM(ticker=game_ticker)
