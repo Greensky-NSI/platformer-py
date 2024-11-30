4661e84b7717164fdfb9cc96bcc276309213b23e
Axelwlr <axel.willier@gmail.com>
2024-11-30 14:23:29 +0100

    Delete src directory

--- src/types/entities.py
+++
@@ -1,3 +0,0 @@
-# Type de hitbox. Le premier nombre est la coordonnée X de départ, le deuxième la coordonnée Y de départ. Départ = en bas à gauche
-# Le troisième nombre est la coordonnée Y d'arrivée, la quatrième la coordonnée Y d'arrivée. Arrivée = en haut à droite
-hitbox_type = tuple[int, int, int, int]
--- src/types/movement.py
+++
@@ -1,3 +0,0 @@
-from typing import Literal
-
-Direction = Literal["left", "right"]
