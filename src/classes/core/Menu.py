b1d26c16b503fa5001b1583378f94c98679fe375
Greensky <david.hesliere@gmail.com>
2024-12-01 21:59:13 +0100

    fix: 🐛 Fix d'affichage des boutons
    Signed-off-by: Greensky <david.hesliere@gmail.com>

--- src/classes/core/Menu.py
+++ src/classes/core/Menu.py
@@ -5,7 +5,7 @@ penser à ajouter chaque niveau dans la dictionnaire niveaux tel que {'n':[lvl_n
 from p5 import *
 
 from src.classes.environnement.Level import Level
-from src.utils.toolbox import safe_fill
+from src.utils.toolbox import safe_fill, hitbox_collide
 from src.utils.globals import env
 from src.custom_types import *
 from src.classes.core.DOM import DOM
@@ -15,29 +15,32 @@ from src.utils.niveaux import niveaux
 class Button:
     def __init__(self, x, y, taille, num_lvl, action=None):
         self.x=x
-        self.y=-y
+        self.y=y
         self.s=taille
         self.a=action
         self.num=num_lvl
         self.hovered = False
 
     def draw_button(self):
-        safe_fill((200 if self.hovered else 255,) * 3)
-        rect(self.x, self.y, self.s, self.s)
+        safe_fill((200 if self.is_mouse_over() else 255,) * 3)
+        rect(self.x, self.y, self.s, -self.s)
         
         # Dessiner le texte
         safe_fill((0, 0, 0))
         text_align(CENTER, CENTER)
-        text(str(self.num), self.x + self.s / 2, self.y + self.s / 2)
+        text(str(self.num), self.x + self.s / 2, self.y - self.s / 2)
 
         self.handle_mouse()
-        
+
+    @property
+    def hitbox(self):
+        return self.x, self.y, self.x + self.s, self.y - self.s
+
     def is_mouse_over(self):
-        return ((self.x <= mouse_x <= self.x + self.s) and
-                (self.y >= mouse_y-600-self.s >= self.y - self.s))
+        return hitbox_collide(self.hitbox, (mouse_x, mouse_y, mouse_x, mouse_y))
     
     def handle_mouse(self):
-        if self.is_mouse_over and mouse_is_pressed:
+        if self.is_mouse_over() and mouse_is_pressed:
             if self.num==1:
                 self.a()
             elif niveaux[self.num-1][1] and self.a is not None:
@@ -59,23 +62,30 @@ class Menu:
     def creer_boutons(self):
         for lvl in niveaux.keys():
             if int(lvl)<=6:
-                self.buttons.append(Button(50+100*(int(lvl)-1), env.height-25, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
+                self.buttons.append(Button(50+100*(int(lvl)-1), 125, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
             elif 6<int(lvl)<=12:
-                self.buttons.append(Button(50+100*(int(lvl)-7), env.height-225, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
+                self.buttons.append(Button(50+100*(int(lvl)-7), 225, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
             elif 12<int(lvl)<=18:
-                self.buttons.append(Button(50+100*(int(lvl)-13), env.height-325, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
+                self.buttons.append(Button(50+100*(int(lvl)-13), 325, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
             elif 18<int(lvl)<=24:
-                self.buttons.append(Button(50+100*(int(lvl)-19), env.height-425, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
+                self.buttons.append(Button(50+100*(int(lvl)-19), 425, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
             elif 24<int(lvl)<=30:
-                self.buttons.append(Button(50+100*(int(lvl)-25), env.height-525, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
+                self.buttons.append(Button(50+100*(int(lvl)-25), 525, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
             elif 30<int(lvl)<=36:
-                self.buttons.append(Button(50+100*(int(lvl)-31), env.height-625, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
+                self.buttons.append(Button(50+100*(int(lvl)-31), 625, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
             elif 36<int(lvl)<=42:
-                self.buttons.append(Button(50+100*(int(lvl)-37), env.height-725, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
+                self.buttons.append(Button(50+100*(int(lvl)-37), 725, 50, int(lvl), lambda: self.set_dom(niveaux[lvl][0], lvl)))
 
     def display_menu(self):
-        translate(0, env.height)
         background(165)
+
+        coef = 3
+        scale(coef)
+        safe_fill((127, 12, 98))
+
+        text("Menu", (env.width - 2 * coef) // (coef * 2), coef * 3)
+
+        scale(1 / coef)
 
         for button in self.buttons:
             button.draw_button()
