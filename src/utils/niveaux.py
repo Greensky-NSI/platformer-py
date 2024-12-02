e51c1d57500d10c5d1e9a401949f7490d54ee82d
greensky-gs <david.hesliere@gmail.com>
2024-12-02 10:43:10 +0100

    chore: 🚀 Ajout de 3 niveaux
    Signed-off-by: greensky-gs <david.hesliere@gmail.com>

--- src/utils/niveaux.py
+++ src/utils/niveaux.py
@@ -1,4 +1,13 @@
 from src.levels.one import level_one as lvl_1
-from src.levels.two import level_two
+from src.levels.two import level_two as lvl_2
+from src.levels.three import level_three as lvl_3
+from src.levels.four import level_four as lvl_4
+from src.levels.five import level_five as lvl_5
 
-niveaux={'1':[lvl_1, False], '2': [level_two, False]}
+niveaux={
+    '1':[lvl_1, False],
+    '2': [lvl_2, False],
+    '3': [lvl_3, False],
+    '4': [lvl_4, False],
+    '5': [lvl_5, False]
+}
